fa226e09a1a97aff32854bc65fe8eedddba0425c
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-18 10:31:08 -0300

    Aula 5.4. react-flash-cards-v2 – Integração do Back End e Front End – parte 2

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -7,57 +7,80 @@ import { useEffect, useState } from 'react';
 import { helperShuffleArray } from '../helpers/arrayHelpers';
 import RadioButton from '../components/RadioButton';
 import { apiGetAllFlashCards } from '../services/apiService';
+import { get } from '../services/httpService';
 
 export default function FlashCardsPage() {
+  // BackEnd
   const [allCards, setAllCards] = useState([]);
+
+  // Exclusivo para "Estudo"
+  const [studyCards, setStudyCards] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
   const [radioButtonShowTitle, setRadioButtonShowTitle] = useState(true);
 
   useEffect(() => {
-    apiGetAllFlashCards().then(allFlashCards => {
-      setAllCards(allFlashCards);
-    });
+    // Sintaxe Promise
+    // apiGetAllFlashCards().then(allFlashCards => {
+    //   setAllCards(allFlashCards);
+    // });
+
+    // Sintaxe async/await
+    async function getAllCards() {
+      const backEndAllCards = await apiGetAllFlashCards();
+      setAllCards(backEndAllCards);
+      setLoading(false);
+    }
+    getAllCards();
+
+    // Sintaxe IIFEE
+    // (async function getAllCards() {
+    //   const backEndAllCards = await apiGetAllFlashCards();
+    //   setAllCards(backEndAllCards);
+    // })();
   }, []);
 
-  function handleButtonClick() {
-    const shuffledCards = helperShuffleArray(allCards);
-    setAllCards(shuffledCards);
+  function handleShuffle() {
+    const shuffledCards = helperShuffleArray(studyCards);
+    setStudyCards(shuffledCards);
   }
+
+  useEffect(() => {
+    setStudyCards(allCards.map(card => ({ ...card, showTitle: true })));
+  }, [allCards]);
 
   function handleRadioShowTitleClick() {
     // prettier-ignore
     const updatedCards = 
-      [...allCards].map(card => ({ ...card, showTitle: true, }));
+      [...studyCards].map(card => ({ ...card, showTitle: true, }));
 
-    setAllCards(updatedCards);
+    setStudyCards(updatedCards);
     setRadioButtonShowTitle(true);
   }
 
   function handleRadioShowDescriptionClick() {
     // prettier-ignore
     const updatedCards = 
-      [...allCards].map(card => ({ ...card, showTitle: false, }));
+      [...studyCards].map(card => ({ ...card, showTitle: false, }));
 
-    setAllCards(updatedCards);
+    setStudyCards(updatedCards);
     setRadioButtonShowTitle(false);
   }
 
   function handleToggleFlashCard(cardId) {
-    const uptadedCards = [...allCards];
+    const uptadedCards = [...studyCards];
     const cardIndex = uptadedCards.findIndex(card => card.id === cardId);
     uptadedCards[cardIndex].showTitle = !uptadedCards[cardIndex].showTitle;
-    setAllCards(uptadedCards);
+    setStudyCards(uptadedCards);
   }
-
-  // useEffect(() => {
-  //   apiGetAllFlashCards().then(setAllCards);
-  // }, []);
 
   return (
     <>
       <Header>react-flash-cards-v2</Header>
       <Main>
         <div className="text-center mb-4">
-          <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+          <Button onButtonClick={handleShuffle}>Embaralhar Cards</Button>
         </div>
         <div className="flex flex-row items-center justify-center space-x-4 m-4">
           <RadioButton
@@ -78,7 +101,7 @@ export default function FlashCardsPage() {
           </RadioButton>
         </div>
         <FlashCards>
-          {allCards.map(({ id, title, description, showTitle }) => {
+          {studyCards.map(({ id, title, description, showTitle }) => {
             return (
               <FlashCard
                 key={id}
