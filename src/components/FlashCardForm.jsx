4a13893d4572816412f087b78a782d815c6018cb
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-24 14:22:37 -0300

    Criar componente Form e montar formulário para inserção/edição de cards reaproveitando o componente TextInput

--- src/components/FlashCardForm.jsx
+++ src/components/FlashCardForm.jsx
@@ -1,4 +1,11 @@
+import TextInput from './TextInput';
+
 export default function FlashCardForm({ createMode = true }) {
   const backgroundClassName = createMode ? 'bg-green-100' : 'bg-yellow-100';
-  return <div className={backgroundClassName}>FlashCardForm</div>;
+  return (
+    <form className={`${backgroundClassName} p-4`}>
+      <h2 className="text-center font-semibold">Manutenção de Flash Cards</h2>
+      <TextInput labelDescription="Título:" />
+    </form>
+  );
 }
